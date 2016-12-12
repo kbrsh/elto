109519d791b23d20157088f87555ed14d8b3cf0a
Gopal Shah <simplecooldude1@gmail.com>
2016-12-11 19:23:09 -0800

    basic error route

--- src/handle.js
+++ src/handle.js
@@ -1,4 +1,8 @@
 var route = require("./route.js");
 module.exports = (req, res) => {
-  route.routes[req.method][req.url](req, res);
+  if(!route.routes[req.method][req.url]) {
+    route.routes.ERROR.notFound(req, res);
+  } else {
+    route.routes[req.method][req.url](req, res);
+  }
 }
