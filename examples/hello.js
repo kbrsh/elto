a21dcaebad81b2e6dc832504f71afb2f224f9e8b
Gopal Shah <simplecooldude1@gmail.com>
2016-12-11 21:22:01 -0800

    use resposne

--- examples/hello.js
+++ examples/hello.js
@@ -5,6 +5,10 @@ app.get("/", function(req, res) {
   res.end("Hello Elto, got: " + req.url + "\n Headers: " + JSON.stringify(req.headers));
 });
 
+app.get("/html", function(req, res) {
+  res.html("<h1>Hello Elto!</h1><br/><p>This is an HTML Page</p>");
+});
+
 app.get("/redirect", function(req, res) {
   res.redirect("http://example.com");
 });
--- src/response.js
+++ src/response.js
@@ -1,11 +1,11 @@
-var req = require("./request");
+var req = require("./request.js");
 module.exports = (res) => {
   res.redirect = function(url) {
     res.writeHead(302, {'Location': url});
     res.end();
   }
   res.html = function(html) {
-    req.set("Content-Type", "text/html");
+    res.set("Content-Type", "text/html");
     res.end(html);
   }
   return res;
