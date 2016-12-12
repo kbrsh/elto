c79372b434848e892c128e8d98e5544a9292a390
Gopal Shah <simplecooldude1@gmail.com>
2016-12-11 20:11:12 -0800

    more tests

--- examples/hello.js
+++ examples/hello.js
@@ -2,7 +2,7 @@ var elto = require("../index.js");
 var app = elto();
 
 app.get("/", function(req, res) {
-  res.end("Hello Elto, got: " + req.url);
+  res.end("Hello Elto, got: " + req.url + "\n Headers: " + JSON.stringify(req.headers));
 });
 
 app.get("/redirect", function(req, res) {
