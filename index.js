a8fdd28a7080bde7853abd95c57129fbfd549c67
Gopal Shah <simplecooldude1@gmail.com>
2016-12-11 19:12:38 -0800

    post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var run = require("./src/run.js");
 var get = require("./src/get.js");
+var get = require("./src/post.js");
 var log = require("./src/log.js");
 
 function Elto(opts) {
@@ -10,6 +11,7 @@ function Elto(opts) {
 }
 
 Elto.prototype.get = get;
+Elto.prototype.post = post;
 Elto.prototype.run = run;
 
 
