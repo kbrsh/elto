75dd462495983c941af4b143dc464ed09fc5ad8a
Gopal Shah <simplecooldude1@gmail.com>
2016-12-11 21:25:11 -0800

    json example

--- examples/hello.js
+++ examples/hello.js
@@ -9,6 +9,13 @@ app.get("/html", function(req, res) {
   res.html("<h1>Hello Elto!</h1><br/><p>This is an HTML Page</p>");
 });
 
+app.get("/json", function(req, res) {
+  res.json({
+    hello: "Elto",
+    arr: ["This", "Is", "JSON"]
+  });
+});
+
 app.get("/redirect", function(req, res) {
   res.redirect("http://example.com");
 });
