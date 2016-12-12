6cb04355ca74203b7788a7975aecf2bc5df827fa
Gopal Shah <simplecooldude1@gmail.com>
2016-12-11 19:24:58 -0800

    service unavailable

--- src/handle.js
+++ src/handle.js
@@ -3,7 +3,9 @@ module.exports = (req, res) => {
   var handler = route.routes[req.method][req.url];
   if(!handler) {
     route.routes.ERROR.notFound(req, res);
-  } else {
+  } else if(handler) {
     handler(req, res);
+  } else {
+    route.routes.ERROR.serviceUnavailable(req, res);
   }
 }
