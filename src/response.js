525e2a6be195108661d73812fc658b22cb10064a
Gopal Shah <simplecooldude1@gmail.com>
2016-12-11 20:14:56 -0800

    html

--- src/response.js
+++ src/response.js
@@ -1,7 +1,12 @@
+var req = require("./request");
 module.exports = (res) => {
   res.redirect = function(url) {
     res.writeHead(302, {'Location': url});
     res.end();
+  }
+  res.html = function(html) {
+    req.set("Content-Type", "text/html");
+    res.end(html);
   }
   return res;
 };
