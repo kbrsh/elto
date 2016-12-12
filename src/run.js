678e37eda5b17c6f0f16fdc0ca6b4793b338067a
Gopal Shah <simplecooldude1@gmail.com>
2016-12-11 19:56:38 -0800

    handle altered versions

--- src/run.js
+++ src/run.js
@@ -6,7 +6,7 @@ var response = require("./response.js");
 module.exports = (port, ip) => {
   log("======= Elto =======", "blue");
   var server = http.createServer(function(req, res) {
-    handle(req, res);
+    handle(request(req), response(res));
   });
   server.listen(port ? port : 3000, ip ? ip : "0.0.0.0");
 }
