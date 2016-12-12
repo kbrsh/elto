e9747863d2c12cb4ff613bcc90f7abe17e2740cd
Gopal Shah <simplecooldude1@gmail.com>
2016-12-11 17:53:23 -0800

    make logging function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var run = require("./src/run.js");
 var log = require("./src/log.js");
 
 function Elto(opts) {
+  this.log("======= Elto =======", "blue");
   this.routes = [];
   if (!(this instanceof Elto)){
     return new Elto(opts);
