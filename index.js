add1115e10bb36469cc7577d4c8f4fd8c18c02a0
Gopal Shah <simplecooldude1@gmail.com>
2016-12-11 21:30:01 -0800

    missing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require("http");
 var run = require("./src/run.js");
 var get = require("./src/get.js");
 var post = require("./src/post.js");
+var missing = require("./src/missing.js");
 var log = require("./src/log.js");
 
 function Elto(opts) {
@@ -12,6 +13,7 @@ function Elto(opts) {
 
 Elto.prototype.get = get;
 Elto.prototype.post = post;
+Elto.prototype.missing = missing;
 Elto.prototype.run = run;
 
 
