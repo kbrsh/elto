d23bf46be44ebcae51fe65f26ff8b155791b3f86
Gopal Shah <simplecooldude1@gmail.com>
2016-12-11 17:49:18 -0800

    require handle in run

--- src/run.js
+++ src/run.js
@@ -1,7 +1,8 @@
 var http = require("http");
+var handle = require("./src/handle.js");
 module.exports = (port, ip) => {
   var server = http.createServer(function(req, res) {
-    this.handle(req, res);
+    handle(req, res);
   });
   server.listen(port ? port : 3000, ip ? ip : "0.0.0.0");
 }
