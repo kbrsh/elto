7eb39068ac853deffa323a55e9c2099bd0c77388
Gopal Shah <simplecooldude1@gmail.com>
2016-12-12 17:47:53 -0800

    no 503

--- src/handle.js
+++ src/handle.js
@@ -3,9 +3,7 @@ module.exports = (req, res) => {
   var handler = route.routes[req.method][req.url];
   if(!handler) {
     route.routes.ERROR.notFound(req, res);
-  } else if(handler) {
-    handler(req, res);
   } else {
-    route.routes.ERROR.serviceUnavailable(req, res);
+    handler(req, res);
   }
 }
